5fa67c5d04a1f7deeff6714fce346526b2105a96
Erangr90 <erangrady3.0@gmail.com>
2025-02-16 20:28:21 +0200

    change hader

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,9 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <h3>
+            kubernetes!!
+          </h3>
           <a
             className="App-link"
             href="https://reactjs.org"
